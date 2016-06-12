b7e1bbf72e3e282380060b571001357914c876a5
svsechen@163.com <svsechen@163.com>
2016-06-12 11:36:47 +0800

    自动部署更改

--- 自动部署.cmd
+++ 自动部署.cmd
@@ -6,6 +6,7 @@ set public_path=D:\git-toulezu\toulezu-public
 @echo [INFO] ------------------------------------------------------------------------执行 hexo g 命令
 D:
 cd %hexo_path%
+call hexo clean
 call hexo g
 
 @echo [INFO] ------------------------------------------------------------------------将 %hexo_public% 下的文件复制到 %public_path%
